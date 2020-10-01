4325b95e0caf59a03165766c8e3e0648a8c8af35
Sylwester12351 <sylgaw98fm@gmail.com>
2020-10-01 13:00:01 +0200

    Initial commit

---
+++ src/main/java/Sylwester/SylwesterWebPage/SylwesterWebPageApplication.java
@@ -0,0 +1,13 @@
+package Sylwester.SylwesterWebPage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SylwesterWebPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SylwesterWebPageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Sylwester/SylwesterWebPage/SylwesterWebPageApplicationTests.java
@@ -0,0 +1,13 @@
+package Sylwester.SylwesterWebPage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SylwesterWebPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
