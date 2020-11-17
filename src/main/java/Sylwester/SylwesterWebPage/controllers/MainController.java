95f8b56f168c9cbf7561fbc9e5545540df52ecbc
Sylwester <Sylgaw98fm@gmail.com>
2020-11-17 23:20:09 +0100

    Revert "Test version"
    This reverts commit f7dbde7a

--- src/main/java/Sylwester/SylwesterWebPage/controllers/MainController.java
+++ src/main/java/Sylwester/SylwesterWebPage/controllers/MainController.java
@@ -14,7 +14,8 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.LocalDate;
 
-import static Sylwester.SylwesterWebPage.weatherPage.ConfWeatcher.*;
+import static Sylwester.SylwesterWebPage.weatherPage.ConfWeather.*;
+
 
 @Controller
 public class MainController {
