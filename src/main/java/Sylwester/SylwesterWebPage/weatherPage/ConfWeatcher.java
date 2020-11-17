87d55ec012fece983337e32b595d969eb27d4bf4
Sylwester <Sylgaw98fm@gmail.com>
2020-11-17 22:56:20 +0100

    Fix 404 error Test version two

---
+++ src/main/java/Sylwester/SylwesterWebPage/weatherPage/ConfWeatcher.java
@@ -0,0 +1,9 @@
+package Sylwester.SylwesterWebPage.weatherPage;
+
+public class ConfWeatcher {
+    public static String City = "Krakow";
+    public static String Country = "pl";
+    public static String Type = "metric";
+    public static String ApiID = "46da914e3b04bc6125fdb728f355cd0e";
+    public static final String SERVICE = "http://api.openweathermap.org/data/2.5/weather?q=";
+}
