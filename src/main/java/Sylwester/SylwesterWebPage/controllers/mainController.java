9316f9cb66568874b83873c1fe91c7b24a4e7436
Sylwester <Sylgaw98fm@gmail.com>
2020-11-17 17:04:35 +0100

    Add weatherApp.html page Add DataBaseTest.html page

--- src/main/java/Sylwester/SylwesterWebPage/controllers/mainController.java
+++ src/main/java/Sylwester/SylwesterWebPage/controllers/mainController.java
@@ -7,5 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class mainController {
     @GetMapping("/")
     public String mainPage(){return "Home";}
+    @GetMapping("/DataTest")
+    public String dataTest(){return "DataBaseTest";}
+    @GetMapping("/WeatherAppOnline")
+    public String Weather(){return "WeatherApp";}
+
 
 }
